1365896d6e74bbde9b6598fdc08185b70116580a
Adi Munawar <munawar.adi31@gmail.com>
2022-05-21 21:57:29 +0700

    pengujian dan penulisan kode untuk fungsi MathBasic.add

--- src/MathBasic.js
+++ src/MathBasic.js
@@ -1,6 +1,16 @@
 const MathBasic = {
-    add: () => {
+    add: (...args) => {
+        if (args.length !== 2) {
+            throw new Error('the add function accepts only two parameters');
+        }
 
+        const [a, b] = args;
+
+        if (typeof a !== 'number' || typeof b !== 'number') {
+            throw new Error('the add function accept only number parameters');
+        }
+
+        return a + b;
     },
     subtract: () => {
 
--- src/MathBasic.test.js
+++ src/MathBasic.test.js
@@ -12,4 +12,26 @@ describe('A MathBasic', () => {
         expect(MathBasic.divide).toBeInstanceOf(Function);
     });
 
-})
+});
+
+describe('A add function', () => {
+    it('should throw error when not given 2 parameters', () => {
+        expect(() => MathBasic.add()).toThrowError();
+        expect(() => MathBasic.add(1)).toThrowError();
+        expect(() => MathBasic.add(1, 2, 3)).toThrowError();
+        expect(() => MathBasic.add(1, 2, 3, 4)).toThrowError();
+    });
+
+    it('should throw error when given non-number parameters', () => {
+        expect(() => MathBasic.add('1', '2')).toThrowError();
+        expect(() => MathBasic.add(true, {})).toThrowError();
+        expect(() => MathBasic.add(null, false)).toThrowError();
+    });
+
+    it('should return a + b when given two number parameters', () => {
+        expect(MathBasic.add(4, 4)).toEqual(8);
+        expect(MathBasic.add(16, 8)).toEqual(24);
+        expect(MathBasic.add(3, 7)).toEqual(10);
+    });
+
+});
